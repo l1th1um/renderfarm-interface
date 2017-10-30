78bbc698f42371a9a9c65bd4e34540ff40e3a6bf
Andri Fachrur Rozie <4r53n1c@gmail.com>
2017-10-30 14:44:42 +0700

    User Activation

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,5 +1,13 @@
 import random
 import re
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 def generate_username(name):
     number = '{:03d}'.format(random.randrange(1, 999))
@@ -21,3 +29,15 @@ def generate_username(name):
         username = (name.strip().lower() + number)
 
     return username
+
+
+def send_mail(to_email, subject, content, mode="text/plain"):
+    sg = sendgrid.SendGridAPIClient(apikey=os.getenv('SENDGRID_API_KEY'))
+    from_email = Email(os.getenv('EMAIL_FROM'))
+    to_email = Email(to_email)
+    subject = subject
+    content = Content(mode, content)
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+
+    return response.status_code
